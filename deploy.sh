22cda42a8e8c0f1c92e92a3a094c4e85338ae144
weihongyu12 <weihongyu@515cn.com>
2018-08-20 14:47:07 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git config user.email "weihongyu12@outlook.com"
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:weihongyu12/waixiubao-fe.github.io.git master
+git push -f https://${GH_TOKEN}@github.com/weihongyu12/waixiubao-fe.github.io.git master
 
 cd -
