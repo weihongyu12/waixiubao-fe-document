d87f2f9279f27b7a2bce718e09787122439b4660
weihongyu12 <weihongyu12@outlook.com>
2019-08-19 14:44:51 +0800

    update package

--- styleguide.config.js
+++ styleguide.config.js
@@ -4,5 +4,8 @@ module.exports = {
   title: 'Default Style Guide',
   defaultExample: true,
   components: 'src/components/**/*.vue',
+  usageMode: 'expand',
+  exampleMode: 'expand',
+  codeSplit: true,
   styleguideDir: 'docs/.vuepress/dist/document/example/vue-style-guide',
 };
